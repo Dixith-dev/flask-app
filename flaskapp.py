6ba4d2bf342b355d44a4a494f13a05072532f05d
Dixith Mediga <103019867+Dixith-dev@users.noreply.github.com>
2024-02-08 17:20:46 +0530

    Add files via upload

---
+++ flaskapp.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/bot')
+def bot_response():
+    return 'This is the bot response!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
